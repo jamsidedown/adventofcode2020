91a4f1dbbe717056388b2f6a82b40322b299b9bc
jamsidedown <46314954+jamsidedown@users.noreply.github.com>
2020-12-05 15:44:18 +0000

    Further improved day 5 solution

--- day_05/solution.py
+++ day_05/solution.py
@@ -2,30 +2,21 @@ from typing import List, Tuple
 
 
 def part_1(seats: List[str]) -> int:
-    return max(get_id(row, col) for row, col in map(parse_seat, seats))
+    return max(map(parse_seat, seats))
 
 
 def part_2(seats: List[str]) -> int:
-    seat_ids = {get_id(row, col) for row, col in map(parse_seat, seats)}
-    for i in range(get_id(127, 7)):
-        if i not in seat_ids:
-            if (i - 1) in seat_ids and (i + 1) in seat_ids:
-                return i
-    return 0
+    sids = set(map(parse_seat, seats))
+    return next(i for i in range(1024) if i not in sids and (i - 1) in sids and (i + 1) in sids)
 
 
-def parse_seat(seat: str) -> Tuple[int, int]:
-    row = int(seat[:7].replace('F', '0').replace('B', '1'), 2)
-    col = int(seat[7:].replace('L', '0').replace('R', '1'), 2)
-    return row, col
-
-
-def get_id(row: int, col: int) -> int:
-    return (row * 8) + col
+def parse_seat(seat: str) -> int:
+    bin_vals = {'F': '0', 'B': '1', 'L': '0', 'R': '1'}
+    return int(''.join(map(bin_vals.get, seat)), 2)
 
 
 if __name__ == '__main__':
     with open('day_05/input.txt', 'r') as f:
         seats = [seat.strip() for seat in f]
-        print(f'Highest seat ID: {part_1(seats)}')
+        print(f'Highest seat id: {part_1(seats)}')
         print(f'Your seat id: {part_2(seats)}')
--- day_05/test_solution.py
+++ day_05/test_solution.py
@@ -1,24 +1,19 @@
 from day_05.solution import parse_seat, part_1
 
 
-def test_parse_seat():
-    seat = 'FBFBBFFRLR'
-    assert parse_seat(seat) == (44, 5)
-
-
 def test_parse_seat_567():
     seat = 'BFFFBBFRRR'
-    assert parse_seat(seat) == (70, 7)
+    assert parse_seat(seat) == 567
 
 
 def test_parse_seat_119():
     seat = 'FFFBBBFRRR'
-    assert parse_seat(seat) == (14, 7)
+    assert parse_seat(seat) == 119
 
 
 def test_parse_seat_820():
     seat = 'BBFFBBFRLL'
-    assert parse_seat(seat) == (102, 4)
+    assert parse_seat(seat) == 820
 
 
 def test_part_1():
