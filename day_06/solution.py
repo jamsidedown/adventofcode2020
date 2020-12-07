c009f6148c6544bbdca917d7c8b984b17c5bc34d
jamsidedown <46314954+jamsidedown@users.noreply.github.com>
2020-12-07 00:07:16 +0000

    Refactored day 6 to a wonderful functional mess

--- day_06/solution.py
+++ day_06/solution.py
@@ -1,36 +1,22 @@
-from typing import List, Set
+from functools import reduce
+from operator import iand, ior
+from typing import List
 
 
-def part_1(filename: str) -> int:
+def part_1(groups: List[List[str]]) -> int:
+    return sum(len(reduce(ior, map(set, group))) for group in groups)
+
+
+def part_2(groups: List[List[str]]) -> int:
+    return sum(len(reduce(iand, map(set, group))) for group in groups)
+
+
+def parse(filename: str) -> List[List[str]]:
     with open(filename, 'r') as f:
-        parsed = []
-        for group in f.read().split('\n\n'):
-            group_answers = set()
-            for person in group.splitlines():
-                answers = set(person.strip())
-                group_answers |= answers
-            parsed.append(group_answers)
-
-    return sum(len(group) for group in parsed)
-
-
-def part_2(filename: str) -> int:
-    with open(filename, 'r') as f:
-        parsed = []
-        for group in f.read().split('\n\n'):
-            group_answers = set()
-            for i, person in enumerate(group.splitlines()):
-                answers = set(person.strip())
-                if i == 0:
-                    group_answers = answers
-                group_answers &= answers
-            parsed.append(group_answers)
-
-    return sum(len(group) for group in parsed)
+        return [group.splitlines() for group in f.read().split('\n\n')]
 
 
 if __name__ == '__main__':
-    p1 = part_1('day_06/input.txt')
-    print(f'Sum of combined answer counts: {p1}')
-    p2 = part_2('day_06/input.txt')
-    print(f'Sum of shared answer counts: {p2}')
+    groups = parse('day_06/input.txt')
+    print(f'Sum of combined answer counts: {part_1(groups)}')
+    print(f'Sum of shared answer counts: {part_2(groups)}')
--- day_06/test_solution.py
+++ day_06/test_solution.py
@@ -1,13 +1,16 @@
-from day_06.solution import part_1, part_2
+from day_06.solution import part_1, part_2, parse
 
 
 def test_part_1_input_1():
-    assert part_1('day_06/test_input_1.txt') == 6
+    groups = parse('day_06/test_input_1.txt')
+    assert part_1(groups) == 6
 
 
 def test_part_1_input_2():
-    assert part_1('day_06/test_input_2.txt') == 11
+    groups = parse('day_06/test_input_2.txt')
+    assert part_1(groups) == 11
 
 
 def test_part_2_input_2():
-    assert part_2('day_06/test_input_2.txt') == 6
+    groups = parse('day_06/test_input_2.txt')
+    assert part_2(groups) == 6
