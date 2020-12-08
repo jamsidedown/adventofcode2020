148ef9740338ecce9289926374f7b9370d2a666e
jamsidedown <46314954+jamsidedown@users.noreply.github.com>
2020-12-08 13:31:26 +0000

    Added refactored vm for day 8 incase it comes up again

--- day_08/solution.py
+++ day_08/solution.py
@@ -1,5 +1,5 @@
 import re
-from typing import List
+from typing import Callable, Dict, List, Set
 
 
 pattern = re.compile(r'^(\w{3}) ([+-]\d+)')
@@ -7,16 +7,19 @@ pattern = re.compile(r'^(\w{3}) ([+-]\d+)')
 
 class Instr:
     def __init__(self, line: str):
-        opcode, value = pattern.match(line).groups()
-        self.opcode = opcode
-        self.value = int(value)
+        if match := pattern.match(line):
+            opcode, value = match.groups()
+            self.opcode = opcode
+            self.value = int(value)
+        else:
+            raise TypeError()
 
 class Vm:
     def __init__(self, instructions: List[Instr]):
         self.instructions = instructions
         self.ptr = 0
         self.acc = 0
-        self.ptrs = set()
+        self.ptrs: Set[int] = set()
         self.looped = False
 
     def step(self):
@@ -38,6 +41,46 @@ class Vm:
                 return self.acc
             self.ptrs.add(self.ptr)
             self.step()
+
+
+class Vm2:
+    def __init__(self, instructions: List[Instr]):
+        self.instructions = instructions
+        self.ptr = 0
+        self.acc = 0
+        self.ptrs: Set[int] = set()
+        self.looped = False
+        self.ops: Dict[str, Callable[[int], None]] = {
+            'nop': self._nop,
+            'acc': self._acc,
+            'jmp': self._jmp,
+        }
+
+    def run(self) -> int:
+        while True:
+            if self.ptr >= len(self.instructions):
+                return self.acc
+
+            if self.ptr in self.ptrs:
+                self.looped = True
+                return self.acc
+
+            self.step()
+
+    def step(self):
+        self.ptrs.add(self.ptr)
+        instr = self.instructions[self.ptr]
+        self.ops[instr.opcode](instr.value)
+
+    def _nop(self, value: int) -> None:
+        self.ptr += 1
+
+    def _acc(self, value: int) -> None:
+        self.acc += value
+        self.ptr += 1
+
+    def _jmp(self, value: int) -> None:
+        self.ptr += value
 
 
 def part_1(instructions: List[Instr]) -> int:
